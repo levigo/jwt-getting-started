b28c62aa3600289bd3f900188d12233de5188179
Florian Stehling <f.stehling@levigo.de>
2019-08-12 10:46:55 +0200

    adjust spring boot tutorial 000 to classic tutorial 002

--- jwt-tutorial-spring-boot-000/src/main/java/org/jwt/client/ApplicationEntryPoint.java
+++ jwt-tutorial-spring-boot-000/src/main/java/org/jwt/client/ApplicationEntryPoint.java
@@ -21,52 +21,51 @@ import com.levigo.jadice.web.conn.client.ServerConnectionBuilder;
  */
 public class ApplicationEntryPoint implements EntryPoint {
 
-  private JadiceWidget jadiceWidget;
-  
-  public void onModuleLoad() {
-    initServerConnection();
-    
-    jadiceWidget = new JadiceWidget();
+	private JadiceWidget jadiceWidget;
 
-    // Adds the jadiceWidget to the "Viewer"-div of the rootpanel (see index.html).
-    RootPanel rootPanel = RootPanel.get("Viewer");
-    rootPanel.add(jadiceWidget);
-    
-    // Finally, load a test document:
-    loadDocument("https://www.levigo.de/fileadmin/download/jadicewebtoolkit.pdf");
-    
-    GWT.log("jwt tutorial loaded");
-  }
-  
-  /**
-   * For the tutorial we deactivate websocket communication as not supported by
-   * jetty.
-   */
-  private void initServerConnection() {
-    try {
-//      new ServerConnectionBuilder().setWebSocketEnabled(false).setServerSentEventsEnabled(false).build();
-    	new ServerConnectionBuilder().setWebSocketEnabled(false).build();
-    } catch (IOException e) {
-      e.printStackTrace();
-      Window.alert("Error initializing server-connection.");
-    }
-  }
+	public void onModuleLoad() {
+		initServerConnection();
 
-  /**
+		jadiceWidget = new JadiceWidget();
+
+		// Adds the jadiceWidget to the "Viewer"-div of the rootpanel (see index.html).
+		RootPanel rootPanel = RootPanel.get("Viewer");
+		rootPanel.add(jadiceWidget);
+
+		// Finally, load a test document:
+		loadDocument("https://www.levigo.de/fileadmin/download/jadicewebtoolkit.pdf");
+
+		GWT.log("jwt tutorial loaded");
+	}
+
+	/**
+	 * For the tutorial we deactivate websocket communication as not supported by
+	 * jetty.
+	 */
+	private void initServerConnection() {
+		try {
+			new ServerConnectionBuilder().setWebSocketEnabled(false).build();
+		} catch (IOException e) {
+			e.printStackTrace();
+			Window.alert("Error initializing server-connection.");
+		}
+	}
+
+	/**
 	 * Loads the document referenced by the passed url.
 	 * 
 	 * @param url
 	 */
-  private void loadDocument(String docURI) {
+	private void loadDocument(String docURI) {
 		Reader r = new Reader();
 		r.append(new UrlSource(docURI));
 		r.complete(new AsyncCallback<Document>() {
-			
+
 			@Override
 			public void onSuccess(Document result) {
 				jadiceWidget.getPageView().setDocument(result);
 			}
-			
+
 			@Override
 			public void onFailure(Throwable caught) {
 				caught.printStackTrace();
